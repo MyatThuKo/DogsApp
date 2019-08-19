b53b16092317132ad4d7747ee4311b3fc6d47f6e
myatthuko <nymyatthuko@gmail.com>
2019-08-18 21:06:46 -0400

    Added Glide for ImageView

--- app/src/main/java/com/example/dogsapp/view/DogsListAdapter.java
+++ app/src/main/java/com/example/dogsapp/view/DogsListAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dogsapp.R;
 import com.example.dogsapp.model.DogBreed;
+import com.example.dogsapp.utils.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,7 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
 
         name.setText(dogsList.get(position).dogBreed);
         lifeSpan.setText(dogsList.get(position).lifeSpan);
+        Util.loadImage(image, dogsList.get(position).imageUrl, Util.getProgressDrawable(image.getContext()));
     }
 
     @Override
